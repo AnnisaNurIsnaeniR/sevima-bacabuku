d747593d45bde3323466bbb8c4a1c154d8443612
AnnisaNurIsnaeniR <isnaa245@gmail.com>
2023-06-25 08:39:34 +0700

    new changes

--- baca.php
+++ baca.php
@@ -34,12 +34,10 @@
                 <table class="table table-hover table-stripped">
                     <thead>
                         <tr>
-                            <td>Judul Buku</td>
-                            <td><?=$dt_buku['judul_buku']?></td>
+                            <td>Judul Buku : <?=$dt_buku['judul_buku']?></td>
                         </tr>
                         <tr>
-                            <td>Deskripsi</td>
-                            <td><?=$dt_buku['deskripsi']?></td>
+                            <td>Deskripsi : <?=$dt_buku['deskripsi']?></td>
                         </tr>
                         <tr>
                             <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
